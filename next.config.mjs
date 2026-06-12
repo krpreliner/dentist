5cdc2bcff5a4debf8343f37c5422c264792513ba
krpreliner <krpreliner@example.com>
2026-06-12 23:52:40 +0530

    fix: resolve Next.js build failure caused by invalid no-lint flag and properly configure eslint ignore

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // While developing locally without a proper image server
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
